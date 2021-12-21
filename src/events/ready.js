2663b4c74608faa562a18130507ae3601a33ed19
Kettra World <95441889+kettraworld@users.noreply.github.com>
2021-12-21 07:48:30 -0300

    Create ready.js

---
+++ src/events/ready.js
@@ -0,0 +1,5 @@
+module.exports = async (client, message) => {
+
+console.log(`${client.user.tag} foi iniciada em ${client.guilds.cache.size} sevidores!\ntendo acesso a ${client.channels.cache.size} canais!\ncontendo ${client.users.cache.size} usuarios!`);
+
+}
