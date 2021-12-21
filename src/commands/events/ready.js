38550cab2e3b7d6a8ea82d2ffb5f1551a4a2c0f8
Kettra World <95441889+kettraworld@users.noreply.github.com>
2021-12-21 07:47:37 -0300

    Delete src/commands/events directory

--- src/commands/events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client, message) => {
-
-console.log(`${client.user.tag} foi iniciada em ${client.guilds.cache.size} sevidores!\ntendo acesso a ${client.channels.cache.size} canais!\ncontendo ${client.users.cache.size} usuarios!`);
-
-}
